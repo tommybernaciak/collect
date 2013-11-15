6e1f7421546a927170857f9adb9e4afe6a040bb9
Tommy <tommybernaciak@gmail.com>
2013-11-15 21:29:31 +0100

    layout modifications - unfinished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Collect - Rails Application"
+    base_title = "ReCollect"
     if page_title.empty?
       base_title
     else

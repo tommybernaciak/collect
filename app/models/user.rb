8bec51d9f047963b2c2e55dbd3e8e986ca752978
Tommy <tommybernaciak@gmail.com>
2013-11-20 11:51:43 +0100

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ActiveRecord::Base
 
 
 	validates_presence_of :email, :nick
+	validates :nick, length: { maximum: 30, minimum: 5 }
+	validates_uniqueness_of :nick
+	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, format: { with: VALID_EMAIL }
+
+
+	ROLES = %w{admin user}
+
 
 	def is_admin?
     	role == "admin"

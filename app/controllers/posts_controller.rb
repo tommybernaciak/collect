cedc991dffb17869b90ba5bffd57d73d9d4746fa
Tommy <tommybernaciak@gmail.com>
2013-12-28 18:30:20 +0100

    adding post from main page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,44 +2,36 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :signed_in_user
 
-  # GET /posts
-  # GET /posts.json
+  # GET
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
+  # GET
   def show
   end
 
-  # GET /posts/new
+  # GET
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
+  # GET
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
+  # POST
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "post created!"
+      redirect_to root_url
+    else
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
+  # PATCH/PUT
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -52,8 +44,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+  # DELETE
   def destroy
     @post.destroy
     respond_to do |format|
@@ -70,6 +61,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:content, :user_id)
+      params.require(:post).permit(:content)
     end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   
   def index
     @posts = Post.all.paginate(page: params[:page])
+    @post = current_user.posts.build if signed_in?
   end
 
   # static pages

fb609e77e1d048a96ae995a2d126fef5b9bb7019
Tommy <tommybernaciak@gmail.com>
2014-01-26 22:44:51 +0100

    layout unfinished

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,9 +2,11 @@ class Album < ActiveRecord::Base
 	has_many :collections
 	has_many :users, through: :collections
 
-	validates_presence_of :title, :artist
+	default_scope -> { order('created_at DESC') }
+
+	validates_presence_of :title, :artist, :year, :format
 	validates :year, format: {with: /\d/, message: "only allows numbers"}
-	validates :format, inclusion: { in: %w(CD DVD LP), message: "%{value} is not a valid format" }
+	validates :format, inclusion: { in: %w(CD 2CD 3CD DVD 2LP LP 12" 10" 7" other), message: "%{value} is not a valid format" }
 
 	attr_accessor :to_buy, :for_sale
 
